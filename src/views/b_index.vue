685fa5d4ecff63edca5c4f166bf41f31337a9dd1
nick wu <kyriewu1229@gmail.com>
2025-02-17 11:45:30 +0800

    update - 更新後台登入頁

--- src/views/b_index.vue
+++ src/views/b_index.vue
@@ -43,82 +43,22 @@ const login = async () => {
       </router-link>
       <h1>後台管理員登入</h1>
       <div class="login-section">
-        <div class="mb-3">
-          <b-field label="使用者名稱">
-            <b-input
-              v-model="username"
-              placeholder="請輸入使用者名稱"
-              size="is-medium"
-            ></b-input>
-          </b-field>
-        </div>
-        <div class="mb-3">
-          <b-field label="使用者密碼">
-            <b-input
-              type="password"
-              password-reveal
-              v-model="password"
-              size="is-medium"
-              placeholder="請輸入密碼"
-              @keydown.enter="login"
-            ></b-input>
-          </b-field>
-        </div>
-        <b-button type="is-primary" outlined @click="login" size="is-large"
-          >登入</b-button
-        >
+        <n-input
+          v-model:value="username"
+          placeholder="請輸入使用者名稱"
+          size="small"
+        />
+        <n-input
+          type="password"
+          show-password-on="mousedown"
+          v-model:value="password"
+          placeholder="請輸入使用者名稱"
+          size="small"
+        />
+        <n-button strong secondary type="info" @click="login" size="large">
+          登入
+        </n-button>
       </div>
     </div>
   </div>
 </template>
-
-<!-- <script>
-export default {
-  data() {
-    return {
-      username: "",
-      password: "",
-      error: false,
-      Verificationcode: [],
-      ajax_url: import.meta.env.VITE_AJAX_URL,
-    };
-  },
-  methods: {
-    async getData() {
-      const response = await fetch(
-        "https://tibamef2e.com/thd103/g2/dist/api/" + "b_login.php",
-        {
-          method: "POST",
-          mode: "cors",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: this.username,
-            password: this.password,
-          }),
-        }
-      )
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          if (data.success == true) {
-            const expirationDate = new Date();
-            expirationDate.setHours(expirationDate.getHours() + 1); // 過期時間設定為一小時後
-            const expires = expirationDate.toUTCString(); // 將過期時間轉換為 UTC 字串;
-            document.cookie = `bUserId=${data.id}; expires=${expires} ; bUserName=${data.username}`;
-            document.cookie = `bUserName=${data.username};expires=${expires} `;
-            this.$router.push("/backend/member_management");
-          } else {
-            this.error = true;
-          }
-        });
-    },
-    changeCode() {
-      this.Verificationcode;
-    },
-  },
-  mounted() {},
-};
-</script> -->
